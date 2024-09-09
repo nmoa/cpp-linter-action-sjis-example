243717a60afde7c4c1cf5bfd4dd6e58e711698e5
Jumpei Yamaguchi <28538293+nmoa@users.noreply.github.com>
2024-09-09 12:27:38 +0900

    Add a message

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "こんにちは！" << std::endl;
+    std::cout << "コメントを追加しました！" << std::endl;
     return 0;
 }
