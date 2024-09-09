ca522512e098839cae1289a3c43ccda19e8e1f9e
Jumpei Yamaguchi <28538293+nmoa@users.noreply.github.com>
2024-09-09 18:17:48 +0900

    Update comment

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World!" << std::endl; /* 適当なコメント */  // 削除可
+    std::cout << "Hello World!" << std::endl; /* 適当なコメント */  //TODO 削除可
     return 0;
 }
